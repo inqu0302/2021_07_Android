01d412d1f5ef3d1cf44531f7f48f1710c2e0f546
inqu0302 <inqu0302@naver.com>
2021-08-11 11:50:53 +0900

    API등록

---
+++ App_04_News/app/src/main/java/com/inqu0302/topnews/config/NaverAPI_Sample.java
@@ -0,0 +1,12 @@
+package com.inqu0302.topnews.config;
+
+/**
+ * 네이버 OpenAPI ID와 Secret를 변경한후
+ * NaverAPI로 파일이름 변경후 빌드
+ */
+public class NaverAPI_Sample {
+
+    public static final String CLIENT_ID = "YOUR NAVER CLIENT ID";
+    public static final String CLIENT_SECRET = "YOUR NAVER CLIENT SECRET";
+    public static final String NAVER_BASE_URL = "https://openapi.naver.com/v1/search/";
+}
